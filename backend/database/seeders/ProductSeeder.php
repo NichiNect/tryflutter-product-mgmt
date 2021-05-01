c29159ea9b3cb46fd31f60da10036a448d381936
NichiNect <yoniwidhi2@gmail.com>
2021-05-01 08:34:43 +0700

    seeder plus

--- backend/database/seeders/ProductSeeder.php
+++ backend/database/seeders/ProductSeeder.php
@@ -16,23 +16,37 @@ class ProductSeeder extends Seeder
     {
         Product::create([
             'nama_product' => 'Processor Intel Core i9 9900K',
-            'description' => 'Processor intel i5 generasi ke 9',
+            'description' => 'Processor intel i5 generasi ke 9 dengan performa yang sangat joss',
             'thumbnail' => '',
             'price' => 2500000,
         ]);
 
         Product::create([
             'nama_product' => 'Processor Intel Core i5 10500K',
-            'description' => 'Processor intel i5 generasi ke 10',
+            'description' => 'Processor intel i5 generasi ke 10 dengan performa yang joss dan dapat dipakai untuk gaming maupun editing',
             'thumbnail' => '',
             'price' => 2700000,
         ]);
 
         Product::create([
             'nama_product' => 'RAM V-Gen Platinum DDR4 2100MHz 8GB',
-            'description' => 'RAM DDR4 2100MHz',
+            'description' => 'RAM DDR4 2100MHz 8GB merk V-Gen Platinum. Lifetime Warranty',
             'thumbnail' => '',
             'price' => 400000,
+        ]);
+
+        Product::create([
+            'nama_product' => 'RAM V-Gen Platinum DDR3 1333MHz 4GB',
+            'description' => 'RAM DDR4 2100MHz 4GB merk V-Gen Platinum. Lifetime Warranty',
+            'thumbnail' => '',
+            'price' => 200000,
+        ]);
+
+        Product::create([
+            'nama_product' => 'Processor Intel Core i5 5500K',
+            'description' => 'Processor intel i5 generasi ke 5 dengan performa yang cukup untuk developer',
+            'thumbnail' => '',
+            'price' => 1300000,
         ]);
     }
 }
